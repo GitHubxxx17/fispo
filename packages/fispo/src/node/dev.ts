4f68fb856aafe85ed0ddcf37538f060b607deca8
“Githubxxx17” <s486275391@outlook.com>
2025-02-24 14:53:57 +0800

    feat: 搭建单元测试和 e2e 测试环境

--- packages/fispo/src/node/dev.ts
+++ packages/fispo/src/node/dev.ts
@@ -1,10 +1,16 @@
 import { createServer as createViteDevServer } from "vite";
 import { pluginIndexHtml } from "./plugins/indexHtml";
 import pluginReact from "@vitejs/plugin-react";
+import { PACKAGE_ROOT } from "./constants";
 
 export async function createDevServer(root = process.cwd()) {
   return createViteDevServer({
     root,
     plugins: [pluginIndexHtml(), pluginReact()],
+    server: {
+      fs: {
+        allow: [PACKAGE_ROOT],
+      },
+    },
   });
 }
